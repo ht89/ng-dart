0782df5590a5f2a28a8c186cb475b054558808cc
Hung Tran <hungtran@bbv.ch>
2018-09-12 13:43:43 +0700

    added players and player components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { GameStartComponent } from './game-start/game-start.component';
+import { PlayersComponent } from './players/players.component';
+import { PlayerComponent } from './players/player/player.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        GameStartComponent
+        GameStartComponent,
+        PlayersComponent,
+        PlayerComponent
     ],
     imports: [
         BrowserModule,
