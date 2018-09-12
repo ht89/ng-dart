2f44f7ae8b4f66d222295ff6851686ec8d562bc1
ht89 <hungtran891989@gmail.com>
2018-09-12 08:56:48 +0700

    displayed the logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameStartComponent } from './game-start/game-start.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameStartComponent
   ],
   imports: [
     BrowserModule
