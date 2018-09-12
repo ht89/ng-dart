9ccbcca942b787013a344317cab4aa7107d62f97
ht89 <hungtran891989@gmail.com>
2018-09-12 09:09:20 +0700

    vertically centered the log and the input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { GameStartComponent } from './game-start/game-start.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    GameStartComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        GameStartComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
