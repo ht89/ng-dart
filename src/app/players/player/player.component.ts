474924fe5a53c8edf3c84b524535982c36a8119c
Hung Tran <hungtran@bbv.ch>
2018-09-12 18:57:42 +0700

    styled the player current scores

--- src/app/players/player/player.component.ts
+++ src/app/players/player/player.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PlayerComponent implements OnInit {
     @Input() playerNum: number;
 
-    remainingScore = 0;
+    currentScore = 0;
 
     constructor() { }
 
