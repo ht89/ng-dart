62bb044ce2e4601acac417117091cda1dbeb0a1c
Hung Tran <hungtran@bbv.ch>
2018-12-03 17:50:46 +0700

    formmatted code

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -3,86 +3,86 @@ import { AppService } from '../app.service';
 import { Player } from './player/player.interface';
 
 @Component({
-    selector: 'app-players',
-    templateUrl: './players.component.html',
-    styleUrls: ['./players.component.scss', '../app.component.scss']
+  selector: 'app-players',
+  templateUrl: './players.component.html',
+  styleUrls: ['./players.component.scss', '../app.component.scss']
 })
 export class PlayersComponent implements OnInit {
-    playersDisplayed = false;
-    players: Player[] = [];
-    gameScore = 0;
+  playersDisplayed = false;
+  players: Player[] = [];
+  gameScore = 0;
 
-    constructor(private appService: AppService) { }
+  constructor(private appService: AppService) { }
 
-    ngOnInit() {
+  ngOnInit() {
+    this.setDefaultPlayers();
+
+    this.appService.subscribe('players', data => {
+      this.displayPlayers(data['gameStarted']);
+
+      if (data['gameScore']) {
+        this.gameScore = data['gameScore'];
+      }
+
+      if (data['playerDeleted']) {
+        const deletedPlayer = this.players.findIndex(player => {
+          return player.id === data['playerDeleted'];
+        });
+        if (deletedPlayer) {
+          this.players.splice(deletedPlayer, 1);
+        }
+      }
+    });
+  }
+
+  private setDefaultPlayers() {
+    this.players = [
+      {
+        id: 1,
+        name: '',
+        scores: []
+      },
+      {
+        id: 2,
+        name: '',
+        scores: []
+      }
+    ];
+
+    for (let i = 1; i <= 24; i++) {
+      this.players[0].scores.push({
+        id: i,
+        value: null
+      });
+      this.players[1].scores.push({
+        id: i,
+        value: null
+      });
+    }
+  }
+
+  private displayPlayers(gameStarted: boolean) {
+    if (gameStarted !== undefined) {
+      this.playersDisplayed = gameStarted;
+
+      if (!gameStarted) {
         this.setDefaultPlayers();
-
-        this.appService.subscribe('players', data => {
-            this.displayPlayers(data['gameStarted']);
-
-            if (data['gameScore']) {
-                this.gameScore = data['gameScore'];
-            }
-
-            if (data['playerDeleted']) {
-                const deletedPlayer = this.players.findIndex(player => {
-                    return player.id === data['playerDeleted'];
-                });
-                if (deletedPlayer) {
-                    this.players.splice(deletedPlayer, 1);
-                }
-            }
-        });
+      }
     }
+  }
 
-    private setDefaultPlayers() {
-        this.players = [
-            {
-                id: 1,
-                name: '',
-                scores: []
-            },
-            {
-                id: 2,
-                name: '',
-                scores: []
-            }
-        ];
+  addPlayer() {
+    this.players.push({
+      id: this.players.length + 1,
+      name: '',
+      scores: []
+    });
 
-        for (let i = 1; i <= 24; i++) {
-            this.players[0].scores.push({
-                id: i,
-                value: null
-            });
-            this.players[1].scores.push({
-                id: i,
-                value: null
-            });
-        }
+    for (let i = 1; i <= 24; i++) {
+      this.players[this.players.length - 1].scores.push({
+        id: i,
+        value: null
+      });
     }
-
-    private displayPlayers(gameStarted: boolean) {
-      if (gameStarted !== undefined) {
-        this.playersDisplayed = gameStarted;
-
-        if (!gameStarted) {
-            this.setDefaultPlayers();
-        }
-    }
-    }
-
-    addPlayer() {
-        this.players.push({
-            id: this.players.length + 1,
-            name: '',
-            scores: []
-        });
-
-        for (let i = 1; i <= 24; i++) {
-            this.players[this.players.length - 1].scores.push({
-                id: i,
-                value: null
-            });
-        }
-    }
+  }
 }
