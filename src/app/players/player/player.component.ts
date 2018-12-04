012f6b3c5da4049da4309ea5ab6fd1135d49b0e9
Hung Tran <hungtran@bbv.ch>
2018-12-04 14:22:24 +0700

    moved the function deletePlayer up to players comp

--- src/app/players/player/player.component.ts
+++ src/app/players/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
 import { Player } from './player.interface';
 import { Score } from '../score/score.interface';
 import { AppService } from '../../app.service';
@@ -11,6 +11,8 @@ import { AppService } from '../../app.service';
 export class PlayerComponent implements OnInit, OnChanges {
   @Input() player: Player;
   @Input() gameScore: number;
+
+  @Output() deletePlayer = new EventEmitter<any>();
 
   remainingScore = 0;
 
@@ -109,10 +111,6 @@ export class PlayerComponent implements OnInit, OnChanges {
 
   containValidScore(score) {
     return score.value !== null && score.value !== undefined;
-  }
-
-  deletePlayer() {
-    this.appService.publish('players', { playerDeleted: this.player.id });
   }
 
   focusOnNextScoreInput(nextScoreId) {
