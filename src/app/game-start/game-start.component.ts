3856e5d9be9995fa6fec565d511d4b3e9d3edcc9
Hung Tran <hungtran@bbv.ch>
2018-12-04 11:38:49 +0700

    cleaned code

--- src/app/game-start/game-start.component.ts
+++ src/app/game-start/game-start.component.ts
@@ -4,55 +4,63 @@ import { Subject } from 'rxjs';
 import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 
 @Component({
-    selector: 'app-game-start',
-    templateUrl: './game-start.component.html',
-    styleUrls: ['./game-start.component.scss', '../app.component.scss']
+  selector: 'app-game-start',
+  templateUrl: './game-start.component.html',
+  styleUrls: ['./game-start.component.scss', '../app.component.scss']
 })
 export class GameStartComponent implements OnInit {
-    gameScore: number;
-    private gameScoreSubject = new Subject<any>();
+  gameScore: number;
+  private gameScoreSubject = new Subject<any>();
 
-    gameStarted = true;
-    readonly gameStartTxt = 'Start Game';
-    readonly gameResetTxt = 'Reset Game';
-    btnTxt = this.gameStartTxt;
+  gameStarted = true;
+  readonly gameStartTxt = 'Start Game';
+  readonly gameResetTxt = 'Reset Game';
+  btnTxt = this.gameStartTxt;
 
-    @ViewChild('gameScoreInput') gameScoreInput: ElementRef;
+  @ViewChild('gameScoreInput') gameScoreInput: ElementRef;
 
-    constructor(private appService: AppService) { }
+  constructor(private appService: AppService) { }
 
-    ngOnInit() {
-        this.gameScoreSubject
-            .pipe(
-                debounceTime(500),
-                distinctUntilChanged(),
-                map(res => {
-                    this.appService.publish('players', { gameScore: this.gameScore });
-                })
-            )
-            .subscribe();
+  ngOnInit() {
+    this.gameScoreSubject
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        map(res => {
+          this.appService.publish('players', { gameScore: this.gameScore });
+        })
+      )
+      .subscribe();
+  }
+
+  init() {
+    if (this.gameStarted) {
+      this.handleStartedGames();
+    } else {
+      this.handleEndedGames();
     }
+  }
 
-    init() {
-        if (this.gameStarted) {
-            if (this.gameScore > 0) {
-                this.btnTxt = this.gameResetTxt;
-                this.gameStarted = false;
+  handleStartedGames() {
+    if (this.gameScore > 0) {
+      this.btnTxt = this.gameResetTxt;
+      this.gameStarted = false;
 
-                this.appService.publish('players', { gameStarted: true, gameScore: this.gameScore });
-            }
-        } else {
-            this.btnTxt = this.gameStartTxt;
-            this.gameStarted = true;
-            this.gameScore = null;
-
-            this.gameScoreInput.nativeElement.focus();
-
-            this.appService.publish('players', { gameStarted: false });
-        }
+      this.appService.publish('players', { gameStarted: true, gameScore: this.gameScore });
     }
+  }
 
-    onGameScoreChange() {
-        this.gameScoreSubject.next(this.gameScore);
-    }
+  handleEndedGames() {
+    this.btnTxt = this.gameStartTxt;
+    this.gameStarted = true;
+    this.gameScore = null;
+
+    this.gameScoreInput.nativeElement.focus();
+
+    this.appService.publish('players', { gameStarted: false });
+  }
+
+  onGameScoreChange() {
+    this.gameScoreSubject.next(this.gameScore);
+  }
 }
